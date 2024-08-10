2ae5dd0872cc2199c0b3403e7a91ca3c57d56da2
Utpal Singh <utpalsingh915@gmail.com>
2024-08-10 18:46:24 +0530

    poc is ready

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from 'react'
+import DashboardLayout from './components/DashboardLayout'
+import { CaptureFinger } from './utiles/scanner'
+
+function App() {
+
+  const [fingImg, setFingimg] = useState("")
+  function getFingerPrint() {
+    //@ts-ignore
+    const fprint = new CaptureFinger(1, 5000)
+    setFingimg(fprint.data.BitmapData)
+    console.log(fprint)
+  }
+
+  return (
+    <DashboardLayout>
+      <div className='flex flex-col space-y-4'>
+        <label className="font-semibold">Finger</label>
+        <button type='button' onClick={getFingerPrint} className="font-semibold bg-blue-500 py-2 w-max px-6 rounded-md text-white">Finger 1</button>
+        {
+          fingImg && (
+            <img src={`data:image/png;base64,${fingImg}`} alt='inf' />
+          )
+        }
+      </div>
+    </DashboardLayout>
+  )
+}
+
+export default App
---
+++ src/utiles/scanner.ts
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-var */
+import $ from "jquery"
+//var uri = "https://localhost:8003/mfs100/"; //Secure
+var uri = "http://localhost:8004/mfs100/"; //Non-Secure
+function GetMFS100Info() {
+    return GetMFS100Client("info");
+}
+
+function GetMFS100KeyInfo(key: any) {
+    var MFS100Request = {
+        "Key": key,
+    };
+    var jsondata = JSON.stringify(MFS100Request);
+    return PostMFS100Client("keyinfo", jsondata);
+}
+function CaptureFinger(quality: number, timeout: number) {
+    var MFS100Request = {
+        "Quality": quality,
+        "TimeOut": timeout
+    };
+    var jsondata = JSON.stringify(MFS100Request);
+    return PostMFS100Client("capture", jsondata);
+}
+
+
+function VerifyFinger(ProbFMR: any, GalleryFMR: any) {
+    var MFS100Request = {
+        "ProbTemplate": ProbFMR,
+        "GalleryTemplate": GalleryFMR,
+        "BioType": "FMR" // you can paas here BioType as "ANSI" if you are using ANSI Template
+    };
+    var jsondata = JSON.stringify(MFS100Request);
+    return PostMFS100Client("verify", jsondata);
+}
+function MatchFinger(quality: any, timeout: any, GalleryFMR: any) {
+    var MFS100Request = {
+        "Quality": quality,
+        "TimeOut": timeout,
+        "GalleryTemplate": GalleryFMR,
+        "BioType": "FMR" // you can paas here BioType as "ANSI" if you are using ANSI Template
+    };
+    var jsondata = JSON.stringify(MFS100Request);
+    return PostMFS100Client("match", jsondata);
+}
+
+
+function PostMFS100Client(method: string, jsonData: string) {
+    var res;
+    $.support.cors = true;
+    var httpStaus = false;
+    $.ajax({
+        type: "POST",
+        async: false,
+        crossDomain: true,
+        url: uri + method,
+        contentType: "application/json; charset=utf-8",
+        data: jsonData,
+        dataType: "json",
+        processData: false,
+        success: function (data: any) {
+            httpStaus = true;
+            res = { httpStaus: httpStaus, data: data };
+        },
+        error: function (jqXHR: any, _ajaxOptions: any, _thrownError: any) {
+            res = { httpStaus: httpStaus, err: getHttpError(jqXHR) };
+        },
+    });
+    return res;
+}
+function GetMFS100Client(method: string) {
+    var res;
+    $.support.cors = true;
+    var httpStaus = false;
+    $.ajax({
+        type: "GET",
+        async: false,
+        crossDomain: true,
+        url: uri + method,
+        contentType: "application/json; charset=utf-8",
+        processData: false,
+        success: function (data: any) {
+            httpStaus = true;
+            res = { httpStaus: httpStaus, data: data };
+        },
+        error: function (jqXHR: any, _ajaxOptions: any, _thrownError: any) {
+            res = { httpStaus: httpStaus, err: getHttpError(jqXHR) };
+        },
+    });
+    return res;
+}
+function getHttpError(jqXHR: { status: number; }) {
+    var err = "Unhandled Exception";
+    if (jqXHR.status === 0) {
+        err = 'Service Unavailable';
+    } else if (jqXHR.status == 404) {
+        err = 'Requested page not found';
+    } else if (jqXHR.status == 500) {
+        err = 'Internal Server Error';
+    } else {
+        err = 'Unhandled Error';
+    }
+    return err;
+}
+
+export { CaptureFinger, VerifyFinger, MatchFinger, GetMFS100Info, GetMFS100KeyInfo }
